032b4dc9f88869267a5959a9f63acb8f5459fba5
archit123 <archit.harsh89@gmail.com>
2016-11-02 17:10:32 -0500

    Very First Commit

---
+++ abc.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class abc {
+	public static void main(String[] args) {
+     ArrayList <Integer> weeklyTemperatures = new 
+     ArrayList <Integer>();
+	
+	weeklyTemperatures.add(78);
+	weeklyTemperatures.add(80);
+	weeklyTemperatures.add(89);
+
+	System.out.println(weeklyTemperatures.get(1));	
+	}
+}
